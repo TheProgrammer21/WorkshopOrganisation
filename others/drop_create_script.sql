56cdf14cac31b1d8ffaae5fd8ede989ccac8f361
TheProgrammer21 <lukaswindisch21@gmail.com>
2020-02-15 12:37:05 +0100

    Changed database schema

--- others/drop_create_script.sql
+++ others/drop_create_script.sql
@@ -17,36 +17,15 @@ DROP DATABASE IF EXISTS `workshop`;
 CREATE DATABASE IF NOT EXISTS `workshop` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `workshop`;
 
--- Exportiere Struktur von Tabelle workshop.class
-DROP TABLE IF EXISTS `class`;
-CREATE TABLE IF NOT EXISTS `class` (
-  `classname` varchar(16) NOT NULL,
-  `active` int(1) NOT NULL DEFAULT 1 COMMENT '1 = active, 0 = inactive',
-  PRIMARY KEY (`classname`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Daten Export vom Benutzer nicht ausgewählt
-
 -- Exportiere Struktur von Tabelle workshop.obligatoryunit
 DROP TABLE IF EXISTS `obligatoryunit`;
 CREATE TABLE IF NOT EXISTS `obligatoryunit` (
   `id` int(4) NOT NULL,
   `startDate` date NOT NULL,
   `endDate` date NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `description` varchar(512) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Daten Export vom Benutzer nicht ausgewählt
-
--- Exportiere Struktur von Tabelle workshop.obligatoryunitclass
-DROP TABLE IF EXISTS `obligatoryunitclass`;
-CREATE TABLE IF NOT EXISTS `obligatoryunitclass` (
-  `obligatoryId` int(11) DEFAULT NULL,
-  `classId` varchar(16) DEFAULT NULL,
-  KEY `FK__obligatoryunit` (`obligatoryId`),
-  KEY `FK__class` (`classId`),
-  CONSTRAINT `FK__class` FOREIGN KEY (`classId`) REFERENCES `class` (`classname`),
-  CONSTRAINT `FK__obligatoryunit` FOREIGN KEY (`obligatoryId`) REFERENCES `obligatoryunit` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Daten Export vom Benutzer nicht ausgewählt
@@ -59,9 +38,20 @@ CREATE TABLE IF NOT EXISTS `user` (
   `lastname` varchar(32) NOT NULL,
   `class` varchar(32) NOT NULL,
   `isAdmin` int(1) NOT NULL DEFAULT 0 COMMENT '0 for no, 1 for is admin',
-  PRIMARY KEY (`username`),
-  KEY `FK_user_class` (`class`),
-  CONSTRAINT `FK_user_class` FOREIGN KEY (`class`) REFERENCES `class` (`classname`)
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+
+-- Exportiere Struktur von Tabelle workshop.userworkshop
+DROP TABLE IF EXISTS `userworkshop`;
+CREATE TABLE IF NOT EXISTS `userworkshop` (
+  `workshopId` int(8) NOT NULL,
+  `userId` varchar(10) NOT NULL,
+  PRIMARY KEY (`workshopId`,`userId`),
+  KEY `FK_userworkshop_user` (`userId`),
+  CONSTRAINT `FK__workshop` FOREIGN KEY (`workshopId`) REFERENCES `workshop` (`id`),
+  CONSTRAINT `FK_userworkshop_user` FOREIGN KEY (`userId`) REFERENCES `user` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Daten Export vom Benutzer nicht ausgewählt
