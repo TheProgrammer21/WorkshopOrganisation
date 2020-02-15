f65b54e241ec89def131dbf4b0456297c4c26642
TheProgrammer21 <lukaswindisch21@gmail.com>
2020-02-15 10:41:04 +0100

    Added database connection

--- backend/server.js
+++ backend/server.js
@@ -9,3 +9,10 @@ var app = express();
 app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
+
+var dbPort = process.env.dbPort || 3306
+var dbUser = process.env.dbUser || "root"
+var dbPassword = process.env.dbPassword || "maria"
+var backendPort = process.env.backendPort || 8085
+
+const pool = mariadb.createPool({ host: "localhost", port: dbPort, user: dbUser, password: dbPassword, connectionLimit: 50, database: "workshop" });
